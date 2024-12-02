05d6cd17ade394ea41683c44042075a59ddf2b69
Pavlin <pavling.georgiev@gmail.com>
2024-12-02 06:57:36 -0500

    All services consul registered and operational test day.

--- forex/cmd/main.go
+++ forex/cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"log"
-
+	"context"
 	"flag"
+	"fmt"
+	"log"
 	"strconv"
+	"time"
 
 	server "github.com/gpr3211/seer/forex/internal/handlers/http"
 	"github.com/gpr3211/seer/forex/internal/handlers/websocket"
+	"github.com/gpr3211/seer/pkg/discovery"
+	"github.com/gpr3211/seer/pkg/discovery/consul"
 )
+
+const serviceName = "forex"
 
 func main() {
 
@@ -19,6 +25,28 @@ func main() {
 	flag.Parse()
 	porto := strconv.Itoa(port)
 	log.Printf("Starting rating service on port %s", porto)
+
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID("forex")
+
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 
 	cfg := websocket.NewConfig()
 	srv := server.NewServer(porto, cfg)
