afab3764fbf6a2e77ab5ca51753084cc734d9a53
Pavlin <pavling.georgiev@gmail.com>
2024-12-02 06:57:36 -0500

    All services consul registered and operational test day.

--- usdata/cmd/main.go
+++ usdata/cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
-	server "github.com/gpr3211/seer/usdata/internal/handlers/http"
-	"github.com/gpr3211/seer/usdata/internal/handlers/websocket"
+	"fmt"
 	"log"
 	"strconv"
+	"time"
+
+	"github.com/gpr3211/seer/pkg/discovery"
+	"github.com/gpr3211/seer/pkg/discovery/consul"
+	server "github.com/gpr3211/seer/usdata/internal/handlers/http"
+	"github.com/gpr3211/seer/usdata/internal/handlers/websocket"
 )
+
+const serviceName = "us-trade"
 
 func main() {
 
@@ -17,6 +25,28 @@ func main() {
 	flag.Parse()
 	porto := strconv.Itoa(port)
 	log.Printf("Starting rating service on port %s", porto)
+
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID("us-trade")
+
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(15 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 
 	cfg := websocket.NewConfig()
 	srv := server.NewServer(porto, cfg)
